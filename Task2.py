11199a95bc2b75d7d459305c976e7522e96bcc5c
ashish zade <93912290+Ashishzade10@users.noreply.github.com>
2025-11-11 05:06:57 +0000

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,1 +1,7 @@
-
+sum=0
+def range_sum(first, last):
+    global sum
+    for i in range(first,last+1):
+        sum=sum+i
+    print(f"The sum of number from {first} and {last} is: ", sum)    
+range_sum(int(input("Enter first number: ")), int(input("Enter last number: ")))
